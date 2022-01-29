baee7f7fe93e3aae57e13ae086a1950f0e116adc
Erin ODea <odea.erin@gmail.com>
2022-01-29 11:21:00 -0600

    add getGuess

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  getGuess: function(){
+    return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
+
+
+console.log(game.getGuess())
