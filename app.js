7a3d8e312153506a7ca694d874ccadb36ebcbc88
Erin ODea <odea.erin@gmail.com>
2022-01-29 12:47:12 -0600

    work out outline

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const game = {
   title: 'Guess the Number!',
+  guess: 1,
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
-  prevGuesses: [],
+  prevGuesses: [''],
   getGuess: function(){
-    while (this.prevGuesses = 0)
+    while (this.prevGuesses = ['']) {
     return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
-  },
+    } 
+  }, 
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  },
+  render: function(){
+    if (this.guess === this.secretNum) {
+      return `Congrats! You guessed the number in ${this.prevGuesses.length}!`
+    } else if (this.guess > this.secretNum) {
+      return `Your guess is too high. Previous guesses ${this.prevGuesses}`
+    } else {
+      return `Your guess is too low. Previous guesses ${this.prevGuesses}`
+    }
   }
 }
 
 
-console.log(game.getGuess(1,2,3))
+console.log(game.play())
