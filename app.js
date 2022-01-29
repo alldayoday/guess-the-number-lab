a9b3e22c5c248c2b6896ed42ed8c0dd186e5488c
Erin ODea <odea.erin@gmail.com>
2022-01-29 15:59:39 -0600

    do while loop in play

--- app.js
+++ app.js
@@ -3,26 +3,32 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: [],
-  prevGuesses: [],
+  prevGuesses: [96,99,52,4],
   play: function() {
+    do {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    return this.secretNum
+      return this.secretNum
+    }
+    while (this.prevGuesses.length === 0);
   },
+
   getGuess: function(){
     while (this.prevGuesses.length === 0) {
     return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
     } 
+    this.prevGuesses.push()
+
   }, 
   render: function(){
-    while ((this.prevGuesses[this.prevGuesses.length -1]) !== this.secretNum)
-    if (this.prevGuesses[this.prevGuesses.length-1] === this.secretNum) {
-      return `Congrats! You guessed the number in ${this.prevGuesses.length} tries!`
-    } else if (this.guess > this.secretNum) {
+    while ((this.prevGuesses[this.prevGuesses.length-1]) !== this.secretNum) {
+    if (this.prevGuesses[this.prevGuesses.length] === this.secretNum) {
+      return `Congrats! You guessed the number in ${this.prevGuesses.length-1} tries!`
+    } else if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
       return `Your guess is too high. Previous guesses ${this.prevGuesses}`
     } else {
       return `Your guess is too low. Previous guesses ${this.prevGuesses}`
-    }
+    } }
   }
 }
 
@@ -33,6 +39,8 @@ console.log(game.play())
 console.log(game.render())
 
 console.log(game.getGuess())
+
+console.log(game.prevGuesses)
 
 
 //added note
