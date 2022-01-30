a1dec03563aa941e211f1c00977e73b206732da0
Erin ODea <odea.erin@gmail.com>
2022-01-30 10:13:15 -0600

    try to run play just once and keep number

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
+  biggestNum: 10,
   smallestNum: 1,
   secretNum: [],
   prevGuesses: [],
@@ -18,39 +18,34 @@ const game = {
     // ***why is it not pushing userGuess to prevGuess**
   }, 
   play: function() {
-    while (this.secretNum = [])  {
+    while (this.prevGuesses.length === 0)  {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
       return this.secretNum
     }
-    while(userGuess !== this.secretNum){
-      this.getGuess()
-  //problem here is i lose the secret num if i push to prevGuesses
-    }
+    return this.getGuess()
   },
 //add a render method play will call after a guess is made - render cannot access play's local variables so pass render args as needed
   render: function(){
-    while (this.prevGuesses !== this.secretNum) {
     if (this.prevGuesses[this.prevGuesses.length-1] === this.secretNum) {
-      return `Congrats! You guessed the number in ${this.prevGuesses.length-1} tries!`
-    } else if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
+      return `Congrats! You guessed the number in ${this.prevGuesses.length} tries!`
+    } if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
       return `Your guess is too high. Previous guesses ${this.prevGuesses.join(' ')}`
     } else {
       return `Your guess is too low. Previous guesses ${this.prevGuesses.join(' ')}`
     } }
-  }
 }
+
 
 
 console.log(game.play())
 console.log(game.getGuess())
-console.log(game.prevGuesses)
+
+game.prevGuesses.push(1)
+
+
+
 console.log(game.render())
-
-game.prevGuesses.push(32)
-game.prevGuesses.push(92)
-
-console.log(game.prevGuesses)
 
 
 
