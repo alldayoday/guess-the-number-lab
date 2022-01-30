2c09a4536216a8874ac69df1e13e21c080e61f03
Erin ODea <odea.erin@gmail.com>
2022-01-30 09:21:58 -0600

    logic of pieces

--- app.js
+++ app.js
@@ -2,45 +2,53 @@ const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
-  secretNum: [],
-  prevGuesses: [96,99,52,4],
+  secretNum: null,
+  prevGuesses: [],
+//added user guess to push from outside and play game
+  userGuess: null,
+//add a prevGuesses property to the game initialized to an empty array
+//From within the `play` method, invoke the `getGuess` method from inside a loop, and add the new guess to the `prevGuesses` array.
+//- Hint: this is an excellent use for a while loop (or even a do...while loop!)
+
+  //add a getGuess method that returns a number betwen smallestNum and biggestNum: while loop works if nothing is in prevGuesses
+  getGuess: function(){
+    while (((this.prevGuesses[this.prevGuesses.length-1]) !== this.secretNum)) {
+    return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
+    } 
+    this.userGuess = parseInt(this.userGuess,10)
+    this.prevGuesses.push(this.userGuess)
+    //parseInt is taking user guess and making it a number, then pushing to prevGuesses 
+    // ***why is it not pushing userGuess to prevGuess**
+  }, 
   play: function() {
-    do {
+    while (this.prevGuesses.length === 0)  {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
       return this.secretNum
     }
-    while (this.prevGuesses.length === 0);
+
+  //problem here is i lose the secret num if i push to prevGuesses
   },
 
-  getGuess: function(){
-    while (this.prevGuesses.length === 0) {
-    return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
-    } 
-    this.prevGuesses.push()
 
-  }, 
+//add a render method play will call after a guess is made - render cannot access play's local variables so pass render args as needed
   render: function(){
-    while ((this.prevGuesses[this.prevGuesses.length-1]) !== this.secretNum) {
-    if (this.prevGuesses[this.prevGuesses.length] === this.secretNum) {
+    while (this.prevGuesses !== this.secretNum) {
+    if (this.prevGuesses[this.prevGuesses.length-1] === this.secretNum) {
       return `Congrats! You guessed the number in ${this.prevGuesses.length-1} tries!`
     } else if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
-      return `Your guess is too high. Previous guesses ${this.prevGuesses}`
+      return `Your guess is too high. Previous guesses ${this.prevGuesses.join(' ')}`
     } else {
-      return `Your guess is too low. Previous guesses ${this.prevGuesses}`
+      return `Your guess is too low. Previous guesses ${this.prevGuesses.join(' ')}`
     } }
   }
 }
 
-console.log(game.prevGuesses.length)
-
 console.log(game.play())
 
-console.log(game.render())
-
 console.log(game.getGuess())
-
-console.log(game.prevGuesses)
+console.log(game.prevGuesses[1,2])
+console.log(game.render())
 
 
 //added note
