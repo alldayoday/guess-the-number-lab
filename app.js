527e49363e69a9b7c12152b699649391890e2b83
Erin ODea <odea.erin@gmail.com>
2022-01-30 11:50:17 -0600

    functional game

--- app.js
+++ app.js
@@ -2,50 +2,47 @@ const game = {
   title: 'Guess the Number!',
   biggestNum: 10,
   smallestNum: 1,
-  secretNum: [],
+  secretNum: null,
   prevGuesses: [],
 //add a prevGuesses property to the game initialized to an empty array
+play: function() {
+  this.secretNum = Math.floor(Math.random() * 
+  (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  console.log(this.secretNum)
+  do {
+    this.prevGuesses.push(this.getGuess())
+    this.render()
+  } while (this.prevGuesses[this.prevGuesses.length-1] !== this.secretNum)
+
+},
+
 //From within the `play` method, invoke the `getGuess` method from inside a loop, and add the new guess to the `prevGuesses` array.
 //- Hint: this is an excellent use for a while loop (or even a do...while loop!)
 
   //add a getGuess method that returns a number betwen smallestNum and biggestNum: while loop works if nothing is in prevGuesses
   getGuess: function(){
-    while (((this.prevGuesses[this.prevGuesses.length-1]) !== this.secretNum)) {
-    return `Enter a guess between ${this.smallestNum} and ${this.biggestNum}`
-    } 
-    this.prevGuesses = parseInt(this.prevGuesses,10)
+    let guess
+    do {
+    guess = parseInt(
+      prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`, guess)
+    )
+  } 
+  while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum)
+
+  return guess
+
     //parseInt is taking user guess and making it a number, then pushing to prevGuesses 
-    // ***why is it not pushing userGuess to prevGuess**
   }, 
-  play: function() {
-    while (this.prevGuesses.length === 0)  {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      return this.secretNum
-    }
-    return this.getGuess()
-  },
 //add a render method play will call after a guess is made - render cannot access play's local variables so pass render args as needed
   render: function(){
     if (this.prevGuesses[this.prevGuesses.length-1] === this.secretNum) {
-      return `Congrats! You guessed the number in ${this.prevGuesses.length} tries!`
-    } if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
-      return `Your guess is too high. Previous guesses ${this.prevGuesses.join(' ')}`
+      alert( `Congrats! You guessed the number in ${this.prevGuesses.length} tries!`)
+    } else if (this.prevGuesses[this.prevGuesses.length-1] > this.secretNum) {
+      alert( `Your guess is too high. Previous guesses ${this.prevGuesses.join(' ')}`)
     } else {
-      return `Your guess is too low. Previous guesses ${this.prevGuesses.join(' ')}`
-    } }
+      alert(`Your guess is too low. Previous guesses ${this.prevGuesses.join(' ')}`)
+    } 
+  }
 }
 
-
-
-console.log(game.play())
-console.log(game.getGuess())
-
-game.prevGuesses.push(1)
-
-
-
-console.log(game.render())
-
-
-
+game.play()
